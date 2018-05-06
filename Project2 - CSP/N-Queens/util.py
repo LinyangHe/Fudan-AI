aca659a2a33fbb77aa4e8920e4d5ebd480beb849
Linyang He <leonlovesjazz@gmail.com>
2018-05-06 16:52:31 +0800

    Update

--- Project2 - CSP/N-Queens/util.py
+++ Project2 - CSP/N-Queens/util.py
@@ -165,3 +165,5 @@ def create_weighted_csp():
     csp.add_unary_factor('B', lambda y : 1.0 / y)
     csp.add_binary_factor('A', 'B', lambda x, y : x != y)
     return csp
+
+create_map_coloring_csp()
